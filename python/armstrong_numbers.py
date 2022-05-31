c99b095bce1902fa6889a281cbaa1cd522fabad1
afelix997 <afelix2013@gmail.com>
2022-05-30 18:11:54 -0700

    answer

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,15 @@
-# How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    res=[]
+    string_form=[str(number) for number in numbers]
+    sum=0
+    for number in string_form:
+        sum=0
+        pow=len(list(number))
+        for digit in number:
+            working_digit=int(digit)**pow
+            sum+=working_digit
+        if sum == int(number):
+            res.append(number)
+      
+    return res
+
